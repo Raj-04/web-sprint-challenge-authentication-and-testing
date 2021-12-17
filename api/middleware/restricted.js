a7e94b692c1771ed4c55d7ec74d6f272f1ba6ace
Raj-04 <1204patel.raj@gmail.com>
2021-12-17 14:26:30 -0500

    done with testing all tests passing

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -4,7 +4,7 @@ const { JWT_SECRET } = require('../../env-connect')
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
   if (token) {
-    jwt.verify(token, JWT_SECRET, (err, decode) => {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
       if (err) {
         next({ 
           status: 401,
