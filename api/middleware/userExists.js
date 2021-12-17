f06c71c2fa45fecf5afd4dd7768bd389d420c1d2
Raj-04 <1204patel.raj@gmail.com>
2021-12-17 13:32:53 -0500

    userExists middleware

---
+++ api/middleware/userExists.js
@@ -0,0 +1,16 @@
+const { findBy } = require('../auth/auth-model')
+
+async function userExists (req, res, next) {
+  const [user] = await findBy({ username: req.body.username })
+  if (user) {
+    req.user = user
+    next()
+  } else {
+    next({
+      status: 401,
+      message: 'invalid credentials'
+    })
+  }
+}
+
+module.exports = userExists
